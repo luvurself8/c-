b6113ef6b670367daf74ee8ebd22ca598e7fc952
luvurself8 <choi.sohyun8@gmail.com>
2023-11-01 01:29:16 +0900

    8. 함수

---
+++ 8th/Class1.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _8th;
+
+class Class1
+{
+    // internal void AddNumber(int v1, int v2)
+    // {
+    //     throw new NotImplementedException();
+    // }
+
+    public int AddNumber(int a, int b){
+        return a+b;
+    }
+
+    public void SoundFunction(){
+        Console.WriteLine("소리를 냅니다");
+    }
+
+    public void ReadNumber(int a){
+        Console.WriteLine("숫자를 출력해주는 함수입니다 " + a);
+    }
+
+}
---
+++ 8th/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _8th;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        int a = 5;
+        int b = 6;
+        
+        // 객체생성 
+        Class1 class1 = new();
+
+        // AddNumber 호출
+        int result = class1.AddNumber(a,b);
+        Console.WriteLine(result);
+
+        // SoundFunction 호출
+        class1.SoundFunction();
+
+        // ReadNumber 호출
+        class1.ReadNumber(a);
+    }
+
+    
+}
